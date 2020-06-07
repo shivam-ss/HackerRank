6268620200bd7fd6c5f63a7eb077f60c5930e57b
Shivam Srivastava <48158198+shivam-ss@users.noreply.github.com>
2020-06-07 21:53:29 +0530

    Create Mini-Max_sum.java

---
+++ Mini-Max_sum.java
@@ -0,0 +1,58 @@
+/* Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+
+For example, . Our minimum sum is  and our maximum sum is . We would print
+
+16 24
+
+
+*/
+
+
+
+============================== SOLUTION ============================
+
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the miniMaxSum function below.
+    static void miniMaxSum(int[] arr) {
+            Arrays.sort(arr);
+            long min=0;
+            long max=0;
+            for(int i=0;i<=arr.length-2;i++){
+                min=min+arr[i];
+            }
+            for(int i=1;i<=arr.length-1;i++){
+                max=max+arr[i];
+            }
+            System.out.print(min+" "+max);
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < 5; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        miniMaxSum(arr);
+
+        scanner.close();
+    }
+}
