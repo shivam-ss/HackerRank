c1ffae97c195439e32c27f0b56ec2d329f3717cd
Shivam Srivastava <48158198+shivam-ss@users.noreply.github.com>
2020-07-04 22:03:01 +0530

    Update Minimum_Swaps2.java

--- Minimum_Swaps2.java
+++ Minimum_Swaps2.java
@@ -10,32 +10,17 @@ public class Solution {
 
     // Complete the minimumSwaps function below.
     static int minimumSwaps(int[] arr) {
-    //creating two arrays:
-          int len=arr.length;
-          int []a=new int[len];
-          int []b= new int[len];
-          //we will store elements in one array and index in next array
-            int count=0;
-          for(int i=0;i<len;i++){
-              b[i]=i+1;
-              if(arr[i]!=b[i]){
-                // int j=arr[i]-1;
-                
-                // int temp=arr[j];
-                // arr[j]=arr[i];
-                // arr[i]=temp;
-                
-                // count++;
-                if(arr[i]<i+1){
-                    
-                }
-
-                  
-              }
               
-          }
-         // System.out.println(count);
-          return count;
+        int count = 0;
+    for(int i=0; i<arr.length; i++){
+        while(arr[i] != i+1) {
+            int temp = arr[i];
+            arr[i] = arr[temp-1];
+            arr[temp-1] = temp;  
+            count ++;
+        }
+    }
+    return count;
                 
 
     }
